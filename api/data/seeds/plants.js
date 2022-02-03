62782c2be27ab31e146c8d6fa298dd95b990e1b9
Oscar Hernandez <oscarh425@outlook.com>
2022-02-03 12:33:00 -0800

    fixed db

--- api/data/seeds/plants.js
+++ api/data/seeds/plants.js
@@ -2,9 +2,15 @@ exports.seed = function(knex) {
     return knex('plants')
     .insert([
         {
-            nickname: 'Madam Stole', species: 'Rose', h20Frequency: 'Daily',
-            nickname: 'Professor Apt', species: 'Leaf', h20Frequency: 'Monthly',
-            nickname: 'Phoenix', species: 'Tree', h20Frequency: 'Never',
-            nickname: 'Lady Elegy', species: 'Lotus', h20Frequency: 'Weekly',
+            nickname: 'Madam Stole', species: 'Rose', h2oFrequency: 'Daily',
+        },
+        {
+            nickname: 'Professor Apt', species: 'Leaf', h2oFrequency: 'Monthly',
+        },
+        {
+            nickname: 'Phoenix', species: 'Tree', h2oFrequency: 'Never',
+        },
+        {
+            nickname: 'Lady Elegy', species: 'Lotus', h2oFrequency: 'Weekly',
         }
     ])}
--- api/plants/plants-model.js
+++ api/plants/plants-model.js
@@ -1,3 +1,10 @@
 const router = require('express').Router()
 
 const Plants = require('./plants-model')
+
+// router.get('/', (req,res,next){
+//     Plants.getAllPlants()
+//         .then(plants => {
+//             res.json(p)
+//         })
+// })
