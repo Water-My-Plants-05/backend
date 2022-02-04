664751db02883063283392943cf4b624ba3710fd
Oscar Hernandez <oscarh425@outlook.com>
2022-02-04 11:46:45 -0800

    wip

--- api/plants/plants-model.js
+++ api/plants/plants-model.js
@@ -2,9 +2,12 @@ const router = require('express').Router()
 
 const Plants = require('./plants-model')
 
-// router.get('/', (req,res,next){
-//     Plants.getAllPlants()
-//         .then(plants => {
-//             res.json(p)
-//         })
-// })
+router.get('/', (req,res,next) =>{
+    Plants.getAllPlants()
+        .then(plants => {
+            res.json(plants)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
